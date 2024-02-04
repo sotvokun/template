2ae49e76eaa64a8ee7487647f2541544f20b1a2e
sotvokun <sotvokun@outlook.com>
2024-02-04 13:32:30 +0800

    feat: template service

--- app/site/service.py
+++ app/site/service.py
@@ -5,8 +5,10 @@ from sqlalchemy.orm import Session
 from .services.database import inject as inject_database
 from .services.scheduler import inject as inject_scheduler, ScheulerType
 from .services.simple_cache import inject as inject_memcache, SimpleCache
+from .services.template import inject as inject_template, Jinja2Templates
 
 
 DatabaseService = Annotated[Session, Depends(inject_database)]
 SchedulerService = Annotated[ScheulerType, Depends(inject_scheduler)]
 MemcacheService = Annotated[type[SimpleCache], Depends(inject_memcache)]
+TemplateService = Annotated[Jinja2Templates, Depends(inject_template)]
---
+++ app/site/services/template.py
@@ -0,0 +1,30 @@
+from os import PathLike
+from typing import Any, Callable, Optional, Sequence
+from fastapi import Request
+from fastapi.templating import Jinja2Templates
+
+
+_templates: Optional[Jinja2Templates] = None
+
+
+def initialize(
+    directory: str | PathLike | Sequence[str | PathLike],
+    *,
+    context_processors: Optional[list[Callable[[Request], dict[str, Any]]]] = None,
+    **env_options: Any,
+):
+    global _templates
+    if _templates is not None:
+        return
+    _templates = Jinja2Templates(
+        directory=directory,
+        context_processors=context_processors,
+        **env_options,
+    )
+
+
+def inject() -> Jinja2Templates:
+    global _templates
+    if _templates is None:
+        raise RuntimeError("Templates not initialized")
+    return _templates
