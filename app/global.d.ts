b94a0e055703dd06d02477f0963b7660c32f1a9d
sotvokun <sotvokun@outlook.com>
2024-06-24 22:03:41 +0800

    feat: remove biome

--- app/global.d.ts
+++ app/global.d.ts
@@ -6,8 +6,6 @@ type Head = {
 
 declare module 'hono' {
   interface ContextRenderer {
-    // biome-ignore lint/style/useShorthandFunctionType: Type
-    // biome-ignore lint/suspicious/noExplicitAny: Type
     (params: string | Promise<string>, head?: Head): Response | Promise<Response>;
   }
 }
